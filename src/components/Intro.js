7b46b1295481f2c9b29d763ef27b2c9cc1ec1245
barkat Mohammed Zineddine <m_barkat@estin.dz>
2023-01-05 01:50:38 +0100

    official_version

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,10 +3,9 @@ import "./Intro.css";
 const Intro = () => {
   return (
     <div id="accueil" className="intro">
-      <h1>Lorem ipsum yaaaaw</h1>
+      <h1>Estin bib</h1>
       <p>
-        “ The greatest glory in living lies not in never falling, but in rising
-        every time we fall”
+        “ Estin Library is a platform that collects all the courses of the Graduate School of Computer and Digital Science and Technology (ESTIN) and that facilitates you to consult these courses in comfort, you will find all the TD, EMD, as well as CORRECTIONS on this virtual library just by clicking on the folder you are looking for”
       </p>
       <img className="bib" src="/estinBib.svg" />
       <img src="/estinBib_mobile.svg" className="bib_mobile" />
